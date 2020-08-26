aa97f92682b04f66721fbef93ac63b5c84c21f84
Justin Lovinger <git@justinlovinger.com>
2020-08-26 18:01:59 -0400

    Finish basic collage

--- src/Main.hs
+++ src/Main.hs
@@ -2,30 +2,80 @@
 
 module Main where
 
+import           Data.Bifunctor                 ( first )
 import           System.Environment             ( getArgs )
-
+import           System.Random                  ( RandomGen
+                                                , getStdGen
+                                                , randomR
+                                                )
 import           Graphics.Image                 ( Array
+                                                , Bilinear(..)
+                                                , Border(..)
                                                 , Image
                                                 , Pixel(..)
                                                 , RGBA
                                                 , VU(..)
+                                                , dims
                                                 , makeImage
-                                                , readImageRGB
+                                                , readImageRGBA
+                                                , resize
+                                                , superimpose
                                                 , writeImage
                                                 )
 
+data Position = UpLeft | DownRight
+
 collage
-  :: Array arr RGBA e => [Image arr cs e] -> (Int, Int) -> Image arr RGBA e
-collage images (x, y) = makeImage (x, y) (\_ -> PixelRGBA 0 0 0 0)
+  :: Array arr RGBA e
+  => RandomGen g => [Image arr RGBA e] -> (Int, Int) -> g -> Image arr RGBA e
+collage _      (0, 0) _ = blank (0, 0)
+-- Select a random image
+-- and fill the remaining space
+-- with a collage of random images.
+collage images (w, h) g = superimpose (oy, ox)
+                                      (collage images (w', h') g'')
+                                      base where
+  (image   , g'      ) = first (fit (w, h)) $ choose images g
+  (ih      , iw      ) = dims image
+  (pos     , g''     ) = choose [UpLeft, DownRight] g'
+  ((ix, iy), (ox, oy)) = case pos of
+    -- If chosen image takes width of canvas,
+    -- shift remaining images down.
+    -- Otherwise,
+    -- shift remaining images right.
+    UpLeft    -> ((0, 0), if iw == w then (0, ih) else (iw, 0))
+    DownRight -> ((h - ih, w - iw), (0, 0))
+  base     = superimpose (ix, iy) image (blank (w, h))
+  -- If chosen image takes width of canvas,
+  -- then we have a horizontal slice to work with.
+  -- Otherwise,
+  -- we have a vertical slice.
+  (w', h') = if iw == w then (w, h - ih) else (w - iw, h)
+
+blank :: Array arr RGBA e => (Int, Int) -> Image arr RGBA e
+blank (w, h) = makeImage (h, w) (\_ -> PixelRGBA 0 0 0 0)
+
+choose :: RandomGen g => [a] -> g -> (a, g)
+choose xs g = (xs !! i, g') where (i, g') = randomR (0, length xs - 1) g
+
+fit :: Array arr cs e => (Int, Int) -> Image arr cs e -> Image arr cs e
+fit (w, h) image = resize Bilinear Edge (nh, nw) image where
+  (ih, iw) = dims image
+  rw       = fromIntegral iw / fromIntegral w
+  rh       = fromIntegral ih / fromIntegral h
+  (nw, nh) = if rw > rh
+    then (w, round $ fromIntegral ih / rw)
+    else (round $ fromIntegral iw / rh, h)
 
 main :: IO ()
 main = do
   args <- getArgs
   let nArgs      = length args
-      x          = read $ head args
-      y          = read $ head $ drop 1 args
+      w          = read $ head args
+      h          = read $ head $ drop 1 args
       imagePaths = take (nArgs - 2 - 1) $ drop 2 args
       outputPath = last args
 
-  images <- sequence $ map (readImageRGB VU) imagePaths
-  writeImage outputPath $ collage images (x, y)
+  images <- sequence $ map (readImageRGBA VU) imagePaths
+  g      <- getStdGen
+  writeImage outputPath $ collage images (w, h) g
