23298a122ef541e865e3976f7ba99b5941d0c145
Justin Lovinger <git@justinlovinger.com>
2020-08-26 13:11:59 -0400

    Format Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -2,19 +2,28 @@
 
 module Main where
 
-import System.Environment (getArgs)
+import           System.Environment             ( getArgs )
 
-import Graphics.Image (Array, Image, Pixel(..), RGBA, VU(..), makeImage, readImageRGB, writeImage)
+import           Graphics.Image                 ( Array
+                                                , Image
+                                                , Pixel(..)
+                                                , RGBA
+                                                , VU(..)
+                                                , makeImage
+                                                , readImageRGB
+                                                , writeImage
+                                                )
 
-collage :: Array arr RGBA e => [Image arr cs e] -> (Int, Int) -> Image arr RGBA e
+collage
+  :: Array arr RGBA e => [Image arr cs e] -> (Int, Int) -> Image arr RGBA e
 collage images (x, y) = makeImage (x, y) (\_ -> PixelRGBA 0 0 0 0)
 
 main :: IO ()
 main = do
   args <- getArgs
-  let nArgs = length args
-      x = read $ head args
-      y = read $ head $ drop 1 args
+  let nArgs      = length args
+      x          = read $ head args
+      y          = read $ head $ drop 1 args
       imagePaths = take (nArgs - 2 - 1) $ drop 2 args
       outputPath = last args
 
