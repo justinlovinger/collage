db33b154caf8b5808f043779eed6ac419a854d33
Justin Lovinger <git@justinlovinger.com>
2020-08-27 17:25:14 -0400

    Refactor `collage`

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,7 @@ import           System.Environment             ( getArgs )
 import           System.Random                  ( RandomGen
                                                 , getStdGen
                                                 , randomR
+                                                , split
                                                 )
 import           Graphics.Image                 ( Array
                                                 , Bilinear(..)
@@ -38,36 +39,40 @@ collage
 collage imagePaths (wt, ht) (w, h) g
   | w <= wt || h <= ht = pure $ blank (w, h)
   | otherwise = do
-    let (imagePath, g') = choose imagePaths g
-    image' <- readImageRGB VS imagePath
+      let ((ga, gb), gc) = first split $ split g
 
-    let image = fit (w, h) image'
-        (ih      , iw      ) = dims image
+      imageA' <- readImageRGB VS $ choose imagePaths ga
+      let imageA   = fit (w, h) imageA'
+          (ah, aw) = dims imageA
 
-        (pos     , g''     ) = choose [UpLeft, DownRight] g'
-        ((ix, iy), (ox, oy)) = case pos of
-          -- If chosen image takes width of canvas,
-          -- shift remaining images down.
-          -- Otherwise,
-          -- shift remaining images right.
-          UpLeft    -> ((0, 0), if iw == w then (0, ih) else (iw, 0))
-          DownRight -> ((h - ih, w - iw), (0, 0))
-        -- If chosen image takes width of canvas,
-        -- then we have a horizontal slice to work with.
+      -- If `imageA` takes width of canvas,
+      -- then we have a horizontal slice to work with.
+      -- Otherwise,
+      -- we have a vertical slice.
+      imageB <- collage
+        imagePaths
+        (wt, ht)
+        (if aw == w then (w, h - ah) else (w - aw, h))
+        gb
+
+      pure $ case choose [UpLeft, DownRight] gc of
+        -- If `imageA` takes width of canvas,
+        -- shift `imageB` down.
         -- Otherwise,
-        -- we have a vertical slice.
-        (w', h') = if iw == w then (w, h - ih) else (w - iw, h)
-
-        base     = superimpose (ix, iy) image (blank (w, h))
-
-    fillImage <- collage imagePaths (wt, ht) (w', h') g''
-    pure $ superimpose (oy, ox) fillImage base
+        -- shift `imageB` right.
+        UpLeft    -> combine (w, h) (0, 0) imageA (if aw == w then (0, ah) else (aw, 0)) imageB
+        DownRight -> combine (w, h) (w - aw, h - ah) imageA (0, 0) imageB
+    where
+      combine (w, h) (ax, ay) imageA (bx, by) imageB =
+        superimpose (by, bx) imageB $
+          superimpose (ay, ax) imageA $
+            blank (w, h)
 
 blank :: Array arr RGB e => (Int, Int) -> Image arr RGB e
 blank (w, h) = makeImage (h, w) (\_ -> PixelRGB 0 0 0)
 
-choose :: RandomGen g => [a] -> g -> (a, g)
-choose xs g = (xs !! i, g') where (i, g') = randomR (0, length xs - 1) g
+choose :: RandomGen g => [a] -> g -> a
+choose xs g = xs !! (fst $ randomR (0, length xs - 1) g)
 
 fit :: Array arr cs e => (Int, Int) -> Image arr cs e -> Image arr cs e
 fit (w, h) image = resize Bilinear Edge (nh, nw) image where
